8ba993b6697169d28f994677c6624639fb4b86fb
Paul Chen <paul.chen03@sap.com>
2017-09-17 00:10:27 +0800

    add problem and solution for "Median of Two Sorted Arrays"

---
+++ javascript/median_of_two_sorted_arrays.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  var nums = nums1.concat(nums2);
+  nums.sort(function(a, b) { return a - b; });
+  var length = nums1.length + nums2.length;
+  if (length % 2 === 0) {
+    return (nums[length / 2 - 1] + nums[length / 2]) / 2;
+  } else {
+    return nums[(length - 1) / 2];
+  }
+};
