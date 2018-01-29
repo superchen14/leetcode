ad005c98bd2de982418a66946502fc5e7a5f1295
Paul Chen <paul.chen03@sap.com>
2018-01-30 06:05:11 +0800

    add problem and solution for "N-Queen-II"

---
+++ javascript/n_queens_ii.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function(n) {
+  const n2 = n * n;
+  const board = (new Array(n2)).fill(false);
+
+  const isPosValid = (row, col) => {
+    // check all rows with col
+    for (let i = 0; i < n; ++i) {
+      if (board[i * n + col] && i !== row) return false;
+    }
+
+    // check all cols with row
+    for (let i = 0; i < n; ++i) {
+      if (board[row * n + i] && i !== col) return false;
+    }
+
+    // check 45 direction
+    var row1 = row >= col ? row - col : 0;
+    var col1 = row >= col ? 0 : col - row;
+    while(row1 < n && col1 < n) {
+      if (row1 !== row && board[row1 * n + col1]) return false;
+      ++row1;
+      ++col1;
+    }
+
+    // check 135 direction
+    var row2 = row + col >= n - 1 ? row + col - n + 1 : 0;
+    var col2 = row + col >= n - 1 ? n - 1 : row + col;
+    while(row2 < n && col2 >= 0) {
+      if (row2 !== row && board[row2 * n + col2]) return false;
+      ++row2;
+      --col2;
+    }
+
+    return true;
+  };
+
+  let solutions = [];
+  findSolutions = row => {
+    for(let col = 0; col < n; ++col) {
+      if (!isPosValid(row, col)) continue;
+
+      board[row * n + col] = true;
+      if (row < n - 1) {
+        findSolutions(row + 1);
+      } else {
+        solutions.push(Array.from(board));
+      }
+      board[row * n + col] = false;
+    }
+  }
+
+  findSolutions(0);
+
+  return solutions.length;
+};
---
+++ problems/n_queens_ii.md
@@ -0,0 +1,5 @@
+Follow up for N-Queens problem.
+
+Now, instead outputting board configurations, return the total number of distinct solutions.
+
+![](https://raw.githubusercontent.com/superchen14/leetcode/master/problems/images/8_queens.png)
