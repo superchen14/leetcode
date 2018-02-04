88c9e2cd4d9c55d530beaa586565620eb150b431
Paul Chen <superchen14@gmail.com>
2018-02-04 13:27:21 +0800

    add solution for "substring with concatenation of all words"

--- javascript/substring_with_concatenation_of_all_words.js
+++ javascript/substring_with_concatenation_of_all_words.js
@@ -4,64 +4,39 @@
  * @return {number[]}
  */
 var findSubstring = function(s, words) {
-  var map = {};
-  var indexes = [];
-
-  var distinctWords = [];
-  for (var i = 0; i < words.length; ++i) {
-    if (distinctWords.indexOf(words[i]) === -1) {
-      distinctWords.push(words[i]);
-    }
-  }
-
-  for (var i = 0; i < s.length; ++i) {
-    distinctWords.forEach(function(word) {
-      if (s.indexOf(word, i) === i) {
-        indexes.push(i);
-        map[i] = word;
+  const getMap = () => {
+    let map = {};
+    words.forEach(word => {
+      if (word in map) {
+        map[word] = map[word] + 1;
+      } else {
+        map[word] = 1;
       }
     });
+    return map;
   }
 
-  var results = [];
-
-  while(indexes.length > 0) {
-    var isValid = true;
-    var index = indexes[0];
-    for (var i = 0; i < words.length - 1; ++i) {
-      if (!map[index + map[index].length]) {
-        isValid = false;
-        break;
+  let wordLength = words[0].length;
+  let results = [];
+  for(let i = 0; i <= s.length - wordLength * words.length; i = i + 1) {
+    let map = getMap();
+    let j = i;
+    while(j <= s.length - wordLength) {
+      const subString = s.substr(j, wordLength);
+      if (subString in map) {
+        if (map[subString] === 1) {
+          delete map[subString];
+        } else {
+          map[subString] = map[subString] - 1;
+        }
+        j = j + wordLength;
       } else {
-        index = index + map[index].length;
+        break;
       }
     }
 
-    if (i < words.length - 1) {
-      indexes = indexes.slice(1);
-      continue;
-    }
-
-    var isValid = true;
-    var candidateWords = words.slice();
-    var index = indexes[0];
-    for (var i = 0; i < words.length; ++i) {
-      var word = map[index];
-      var indexInCandidate = candidateWords.indexOf(word);
-      if (indexInCandidate === -1) {
-        isValid = false;
-        break;
-      } else {
-        candidateWords.splice(indexInCandidate, 1);
-        index = index + map[index].length;
-      }
-    }
-
-    if (isValid) results.push(indexes[0]);
-    indexes.shift();
+    if (Object.keys(map).length === 0) results.push(i);
   }
 
   return results;
 };
-
-findSubstring("barfoothefoobarman", ["foo","bar"]);
