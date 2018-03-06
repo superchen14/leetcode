6528988e0003377f191a34fc63e901b5b5ee55a5
Paul Chen <paul.chen03@sap.com>
2018-03-06 16:19:16 +0800

    add problem and solution for "Subsets II"

---
+++ javascript/subsets_ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+  nums = nums.sort();
+  if (nums.length === 0) return [[]];
+  if (nums.length === 1) return [[], nums];
+
+  let previousNums = nums.slice(0, nums.length - 1);
+  let previousSubsets = subsetsWithDup(previousNums);
+
+  const lastNum = nums[nums.length - 1];
+  if (lastNum !== nums[nums.length - 2]) {
+    return previousSubsets.concat(previousSubsets.map(subset => subset.concat(lastNum)));
+  } else {
+    let index = 0;
+    while(nums[index] !== lastNum) ++index; // if lastNum is 5, find index of first 5 
+    const count = nums.length - index;  // if lastNum is 5, how many 5 in nums
+    const subSubsets = subsetsWithDup(nums.slice(0, index));
+    return previousSubsets.concat(subSubsets.map(subSubset => {
+      let temp = count;
+      while(temp > 0) {
+        subSubset.push(lastNum);
+        --temp;
+      }
+      return subSubset;
+    }));
+  }
+};
