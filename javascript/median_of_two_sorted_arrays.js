61bda770dca2009a8b29d3e7e3d78c9537ca19ec
Paul Chen <superchen14@gmail.com>
2018-03-16 00:00:10 +0800

    refine 4 ~ 6

--- javascript/median_of_two_sorted_arrays.js
+++ javascript/median_of_two_sorted_arrays.js
@@ -4,9 +4,9 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-  var nums = nums1.concat(nums2);
+  let nums = nums1.concat(nums2);
   nums.sort(function(a, b) { return a - b; });
-  var length = nums1.length + nums2.length;
+  const length = nums1.length + nums2.length;
   if (length % 2 === 0) {
     return (nums[length / 2 - 1] + nums[length / 2]) / 2;
   } else {
--- javascript/zigzag_conversion.js
+++ javascript/zigzag_conversion.js
@@ -6,24 +6,22 @@
 var convert = function(s, numRows) {
   if (numRows === 1) { return s; }
 
-  var total = 2 * numRows - 2;
+  const total = 2 * numRows - 2;
   function getRow(index) {
-    var remain = index % total;
+    const remain = index % total;
     return remain < numRows ? remain : total - remain;
   }
 
-  var rowStrings = {};
-  for (var index = 0; index < s.length; ++index) {
-    var row = getRow(index);
+  let rowStrings = {};
+  for (let index = 0; index < s.length; ++index) {
+    const row = getRow(index);
     rowStrings[row] = (rowStrings[row] || "") + s[index];
   }
 
-  var returnValue = "";
-  for (var index = 0; index < numRows; ++index) {
+  let returnValue = "";
+  for (let index = 0; index < numRows; ++index) {
     returnValue += (rowStrings[index] || "");
   }
 
   return returnValue;
 };
-
-convert("", 1);
