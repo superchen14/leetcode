f65c257607d7e79f4e2ce2074198cff3089ad7f0
Paul Chen <superchen14@gmail.com>
2018-03-17 14:19:47 +0800

    refine 10 ~ 12

--- javascript/container_with_most_water.js
+++ javascript/container_with_most_water.js
@@ -3,25 +3,25 @@
  * @return {number}
  */
 var maxArea = function(heights) {
-  var i = 0;
-  var j = heights.length - 1;
+  let i = 0;
+  let j = heights.length - 1;
 
-  var getVolume = function(i, j) {
-    var height = heights[i] < heights[j] ? heights[i] : heights[j];
+  const getVolume = (i, j) => {
+    const height = heights[i] < heights[j] ? heights[i] : heights[j];
     return height * (j - i);
   }
 
-  var maxVolume = getVolume(i, j);
+  let maxVolume = getVolume(i, j);
   while(true) {
-    var iHeight = heights[i];
-    var jHeight = heights[j];
+    const iHeight = heights[i];
+    const jHeight = heights[j];
     if (iHeight < jHeight) {
       while(iHeight > heights[++i]) {}
     } else {
       while(jHeight > heights[--j]) {}
     }
     if (i >= j) break;
-    var volume = getVolume(i, j);
+    const volume = getVolume(i, j);
     if (volume > maxVolume) maxVolume = volume;
   }
 
--- javascript/integer_to_roman.js
+++ javascript/integer_to_roman.js
@@ -3,13 +3,15 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-  var values  = [1000, 900,  500,  400,  100,   90,  50,   40,  10,    9,   5,    4,   1];
-  var symbols = [ "M", "CM", "D", "CD",  "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]; 
-  var roman = "";
+  const values  = [1000, 900,  500,  400,  100,   90,  50,   40,  10,    9,   5,    4,   1];
+  const symbols = [ "M", "CM", "D", "CD",  "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]; 
+  let roman = "";
   for(var i = 0; num > 0; ++i) {
-    while(num >= values[i]) {
-      num = num - values[i];
+    let times = Number.parseInt(num / values[i]);
+    num = num - times * values[i];
+    while(times > 0) {
       roman += symbols[i];
+      --times;
     }
   }
 
