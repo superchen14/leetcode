3a578963b359a1862544770d3baab79590da86e9
Paul Chen <superchen14@gmail.com>
2018-03-25 18:57:33 +0800

    refine 31 ~ 33

--- javascript/longest_valid_parentheses.js
+++ javascript/longest_valid_parentheses.js
@@ -3,48 +3,25 @@
  * @return {number}
  */
 var longestValidParentheses = function(s) {
-  let stack = [];
-  const isNumber = char => char !== "(" && char !== ")";
-  const merge = stack => {
-    if (stack.length <= 1) return;
+  let stack = [-1];
 
-    const lastIndex = stack.length - 1;
-    if (isNumber(stack[lastIndex]) && isNumber(stack[lastIndex - 1])) {
-      stack[lastIndex - 1] = stack[lastIndex] + stack[lastIndex - 1];
-      stack.pop();
-      return merge(stack);
-    } else {
-
-    }
-  }
-
-  for (let i = 0; i < s.length; ++i) {
-    if (s[i] === "(") {
-      stack.push(s[i]);
-    } else {
-      if (stack.length >= 1 && stack[stack.length - 1] === "(") {
-        stack.pop();
-        stack.push(2);
-        merge(stack);
-      } else if (stack.length >= 2 && stack[stack.length - 2] === "(") {
-        const length = stack.pop();
-        stack.pop();
-        stack.push(length + 2);
-        merge(stack);
-      } else {
-        stack.push(s[i]);
-      }
-    }
-  }
+  let last = array => array[array.length - 1];
 
   let maxLength = 0;
-  for(let i = 0; i < stack.length; ++i) {
-    if (isNumber(stack[i]) && stack[i] > maxLength) {
-      maxLength = stack[i];
+  for (let i = 0; i < s.length; ++i) {
+    const char = s[i];
+    if (char === "(") {
+      stack.push(i);
+    } else {
+      if (s[last(stack)] === "(") {
+        stack.pop();
+        const currentMaxLength = i - last(stack);
+        if (currentMaxLength > maxLength) maxLength = currentMaxLength;
+      } else {
+        stack.push(i);
+      }
     }
   }
 
   return maxLength;
 };
-
-longestValidParentheses(")()())()()(");
--- javascript/search_in_rotated_sorted_array.js
+++ javascript/search_in_rotated_sorted_array.js
@@ -4,9 +4,33 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  for(let i = 0; i < nums.length; ++i) {
-    if (nums[i] === target) return i;
+  const binarySearch = (start, end) => {
+    // TODO: should use truely binary search implementation
+    if (start === end) return -1;
+    if (start + 1 === end) return nums[start] === target;
+    if (target < nums[start]) return -1;
+    if (target > nums[end - 1]) return -1;
+    for (let i = start; i < end; ++i) {
+      if (nums[i] === target) return i;
+    }
+    return -1;
   }
 
-  return -1;
+  const _search = (start, end) => {
+    if (start === end) return -1;
+    if (start + 1 === end) return nums[start] === target ? start : -1;
+    if (nums[start] < nums[end - 1]) {
+      // start to end is sorted sub-array
+      return binarySearch(start, end);
+    } else {
+      // start to end is rotated sub-array
+      const mid = Number.parseInt((start + end) / 2);
+      if (target === nums[mid]) return mid;
+      const index1 = _search(start, mid);
+      if (index1 !== -1) return index1;
+      return _search(mid + 1, end);
+    }
+  }
+
+  return _search(0, nums.length);
 };
